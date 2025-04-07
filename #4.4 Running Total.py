c7bd6b34f2985d2e1e908851ea7fdefbd8a6e657
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-07 08:26:16 -0400

    Update #4.4 Running Total.py

--- #4.4 Running Total.py
+++ #4.4 Running Total.py
@@ -1,25 +1,25 @@
-#4.4 X
-#4.5 X
-#4.7
-#4.8 
-#while vs for
-#how to find days hours minutes seconds from given seconds
-
-
-
+#constant maximum
 MAX = 5
 
+#running total
 total = 0.0
 
+#prints instructions to user
 print('This program calculates the sum of ', end = '')
 print(f'{MAX} numbers you will enter')
 
+#for loop that will iterate 5 times
 for counter in range(MAX):
+
+    #ask the user for a number
     number = int(input('Enter: '))
 
-    print("Total is ", total)
+    #print("Total is ", total)
+
+    #the new value of total, is the old value plus the number
     total = total + number
-    print("Total has been changed to", total)
 
+    #print("Total has been changed to", total)
+
+#print total
 print(f'The total is {total}')
-
