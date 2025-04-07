1263ab1f66e34608df9ed13e285608cd614f8c3d
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-07 08:25:49 -0400

    Update #4.7 Nested Loops.py

--- #4.7 Nested Loops.py
+++ #4.7 Nested Loops.py
@@ -1,5 +1,8 @@
-
+#outer loop will iterate 60 times
 for minutes in range(60):
-    
+
+    #each time, this loop will iterate 60 times.
     for seconds in range(60):
         print(minutes, ':', seconds)
+
+#for every time the minute loop iterates, the seconds loop will iterate 60 times.
