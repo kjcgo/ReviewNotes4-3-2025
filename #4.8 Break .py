6d46ac8eb70988bda675b0b722aaaf68b27b4d53
kjcgo <113406963+kjcgo@users.noreply.github.com>
2025-04-07 08:29:13 -0400

    Update #4.8 Break .py

--- #4.8 Break .py
+++ #4.8 Break .py
@@ -1,10 +1,17 @@
+#start at 0
 n= 0
+
+#keep looping while n is less than 100
 while n < 100:
     print(n)
 
+    #if n does equal 5, break out of the loop
+    #the loop will stop iterating and go to the next line of code
     if n == 5:
         break
 
+    #add 1 to n
     n += 1
 
+#show when the loop stopped
 print(f'The loop stopped and n is {n}')
